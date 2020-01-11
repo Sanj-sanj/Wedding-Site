6a3e461d4788c46961b0f3781b297f756bf651f7
Sanj-sanj <45620169+Sanj-sanj@users.noreply.github.com>
2020-01-10 20:35:04 -0800

    Add files via upload

---
+++ sitejs.js
@@ -0,0 +1,36 @@
+let toggle = false; 
+let navToggle = document.getElementById('js-mobile-nav');
+let header = document.getElementById('top');
+let mobileLinks = document.getElementById('js-mobile-links');
+
+
+document
+.querySelector('.button')
+.addEventListener("click", function() {
+    buttonClick(navToggle.display);
+})
+
+function buttonClick() {
+    navToggle.scrollIntoView();
+    if (toggle != true) {
+        foo();
+        toggle = true;        
+    } 
+    else  {
+        bar();
+        toggle = false;
+    }    
+}
+
+function foo () {
+    //navToggle.style.display = 'flex';
+    header.style.marginBlockStart = '9.8%';
+    navToggle.classList.add('mobile-nav-active')
+    navToggle.style.height = ('200px')
+}
+
+function bar() {
+    //navToggle.style.display = 'none';
+    navToggle.style.height = ('0px');
+    header.style.marginBlockStart = '9.8%';
+}
